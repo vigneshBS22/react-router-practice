40415e601f78fc024bd4b48c4594f04c47d4f947
vigneshBS22 <vigneshb@geekyants.com>
2021-11-19 15:28:42 +0530

    added data

--- src/data.js
+++ src/data.js
@@ -1,91 +1,91 @@
 export const data1 = [
   {
-    name: 'burgers',
+    name: 'Burgers',
     img: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
     id: 1,
     items: [
       {
         id: 1,
         img: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Burger',
+        name: 'Special Burger',
         content:
           'Our Burger is a ¼ lb* of savory flame-grilled paneer topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun.',
         price: 100,
       },
       {
         id: 2,
-        img: 'https://images.unsplash.com/photo-1547496502-affa22d38842?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Salad',
+        img: 'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        name: "Chef's choice burger",
         content:
-          'A cold dish of various mixtures of raw or cooked vegetables, usually seasoned with oil, vinegar, or other dressing and sometimes accompanied by meat, fish, or other ingredients.',
+          'Our Burger is a ¼ lb* of savory flame-grilled paneer topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun.',
         price: 150,
       },
       {
         id: 3,
-        img: 'https://images.unsplash.com/photo-1579954115563-e72bf1381629?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGljZSUyMGNyZWFtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Ice cream',
+        img: 'https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        name: 'Half pound burger',
         content:
-          'Ginger bananas infused with hot buttered rum, and topped with delicious chocolate ice cream, warm your heart from the inside out.',
+          'Our Burger is a ¼ lb* of savory flame-grilled paneer topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun.',
         price: 200,
       },
     ],
   },
   {
     id: 2,
-    name: 'salads',
+    name: 'Salads',
     img: 'https://images.unsplash.com/photo-1547496502-affa22d38842?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
     items: [
       {
         id: 1,
-        img: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Burger',
+        img: 'https://images.unsplash.com/photo-1547496502-affa22d38842?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        name: 'Green salad',
         content:
-          'Our Burger is a ¼ lb* of savory flame-grilled paneer topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun.',
+          'A cold dish of various mixtures of raw or cooked vegetables, usually seasoned with oil, vinegar, or other dressing and sometimes accompanied by meat, fish, or other ingredients.',
         price: 100,
       },
       {
         id: 2,
-        img: 'https://images.unsplash.com/photo-1547496502-affa22d38842?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Salad',
+        img: 'https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        name: 'Italian Salad',
         content:
           'A cold dish of various mixtures of raw or cooked vegetables, usually seasoned with oil, vinegar, or other dressing and sometimes accompanied by meat, fish, or other ingredients.',
         price: 150,
       },
       {
         id: 3,
-        img: 'https://images.unsplash.com/photo-1579954115563-e72bf1381629?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGljZSUyMGNyZWFtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Ice cream',
+        img: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        name: 'Vegetable salad',
         content:
-          'Ginger bananas infused with hot buttered rum, and topped with delicious chocolate ice cream, warm your heart from the inside out.',
+          'A cold dish of various mixtures of raw or cooked vegetables, usually seasoned with oil, vinegar, or other dressing and sometimes accompanied by meat, fish, or other ingredients.',
         price: 200,
       },
     ],
   },
   {
     id: 3,
-    name: 'salads',
+    name: 'Ice creams',
     img: 'https://images.unsplash.com/photo-1579954115563-e72bf1381629?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGljZSUyMGNyZWFtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
     items: [
       {
         id: 1,
-        img: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Burger',
+        img: 'https://images.unsplash.com/photo-1603384358453-e337643f75fa?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTJ8fGljZSUyMGNyZWFtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        name: 'Ice cream sundae',
         content:
-          'Our Burger is a ¼ lb* of savory flame-grilled paneer topped with juicy tomatoes, fresh lettuce, creamy mayonnaise, ketchup, crunchy pickles, and sliced white onions on a soft sesame seed bun.',
+          'Ginger bananas infused with hot buttered rum, and topped with delicious chocolate ice cream, warm your heart from the inside out.',
         price: 100,
       },
       {
         id: 2,
-        img: 'https://images.unsplash.com/photo-1547496502-affa22d38842?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Salad',
+        img: 'https://media.istockphoto.com/photos/ice-cream-scoops-in-cones-with-copy-space-on-blue-picture-id1222009180?b=1&k=20&m=1222009180&s=170667a&w=0&h=Wiks_1uzl89NxXuyGWLWTOA_MWDcneKKvg8kcpEf_OU=',
+        name: 'Cone ice cream',
         content:
-          'A cold dish of various mixtures of raw or cooked vegetables, usually seasoned with oil, vinegar, or other dressing and sometimes accompanied by meat, fish, or other ingredients.',
+          'Ginger bananas infused with hot buttered rum, and topped with delicious chocolate ice cream, warm your heart from the inside out.',
         price: 150,
       },
       {
         id: 3,
         img: 'https://images.unsplash.com/photo-1579954115563-e72bf1381629?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGljZSUyMGNyZWFtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-        name: 'Ice cream',
+        name: 'Ginger banana ice cream',
         content:
           'Ginger bananas infused with hot buttered rum, and topped with delicious chocolate ice cream, warm your heart from the inside out.',
         price: 200,
