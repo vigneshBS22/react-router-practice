d9a871c9e548c9d430906429b941a0ebcccaad86
vigneshBS22 <vigneshb@geekyants.com>
2021-11-19 17:08:50 +0530

    docs: removed useNavigate hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
-  const navigate = useNavigate();
   return (
     <div className='flex gap-4 shadow-2xl bg-white'>
       <NavLink
