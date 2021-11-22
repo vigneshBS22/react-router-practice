c7e76c3e12f160ce972934183be65562d162c41a
vigneshBS22 <vigneshb@geekyants.com>
2021-11-22 10:46:42 +0530

    style: corrected margin in home apge

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import { HOC } from '../components/HOC';
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className='mt-96'>
+    <div>
       <div>Explore delicious food items which are nutritious and healthy</div>
       <button
         className='rounded-2xl bg-yellow-500 hover:bg-red-300 text-white border-opacity-100 p-2 mt-2 transform hover:scale-105 hover:shadow-2xl'
